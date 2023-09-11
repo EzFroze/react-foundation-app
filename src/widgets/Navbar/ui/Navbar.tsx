7a41fe7e40990ef7d3831c1bac6bf1eafb0b9836
rusultanbekov <sultanbekov-01@yandex.ru>
2023-09-12 00:51:18 +0300

    update ui tests

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,21 +1,15 @@
 import React, { type FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import styles from './Navbar.module.scss';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string
 }
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
-  const { t } = useTranslation();
   return (
     <div className={classNames(styles.Navbar, {}, [className])}>
-      <div className={styles.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to='/' className={styles.mainLink}>{t('toMainPage')}</AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to='/about'>{t('toAboutPage')}</AppLink>
-      </div>
+      <div className={styles.links}></div>
     </div>
   );
 };
