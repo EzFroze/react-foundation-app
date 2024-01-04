b8189fb6f710ed37771690fe28acf921c63fdc28
rusultanbekov <sultanbekov-01@yandex.ru>
2024-01-05 01:42:58 +0300

    added tree shaking

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,21 @@ import { StoreProvider } from "app/providers/StoreProvider";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import { render } from "react-dom";
 import "./app/styles/index.scss";
+import { I18nextProvider } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
-
-import "shared/config/i18n/i18n";
+import i18n from "shared/config/i18n/i18n";
 
 render(
-  <BrowserRouter>
-    <StoreProvider>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </StoreProvider>
-  </BrowserRouter>,
+  <I18nextProvider i18n={i18n}>
+    <BrowserRouter>
+      <StoreProvider>
+        <ErrorBoundary>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </ErrorBoundary>
+      </StoreProvider>
+    </BrowserRouter>
+  </I18nextProvider>,
   document.getElementById("root")
 );
