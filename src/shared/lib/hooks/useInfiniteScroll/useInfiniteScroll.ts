6dd5cf5e4f8a723d106cd54248ce2211bae20e9d
rusultanbekov <sultanbekov-01@yandex.ru>
2024-04-09 04:01:21 +0300

    added infinite scroll

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -12,6 +12,9 @@ export function useInfiniteScroll({
   triggerRef,
 }: UseInfiniteScrollOptions) {
   useEffect(() => {
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
+
     let observer: IntersectionObserver | null = null;
 
     if (!callback) {
@@ -19,7 +22,7 @@ export function useInfiniteScroll({
     }
 
     const options = {
-      root: wrapperRef.current,
+      root: wrapperElement,
       rootMargin: "0px",
       threshold: 1.0,
     };
@@ -30,12 +33,11 @@ export function useInfiniteScroll({
       }
     }, options);
 
-    observer.observe(triggerRef.current);
+    observer.observe(triggerElement);
 
     return () => {
-      if (observer) {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        observer.unobserve(triggerRef.current);
+      if (observer && triggerElement) {
+        observer.unobserve(triggerElement);
       }
     };
   }, [callback, triggerRef, wrapperRef]);
